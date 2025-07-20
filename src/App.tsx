2c7344c3e1ab9e5fcb18cba6f464835a5eb61378
GiangNT <158981273+giang-tourchain@users.noreply.github.com>
2025-07-20 18:23:03 +0700

    Update basename for Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/proplan-hub">
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Index />} />
